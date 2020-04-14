b887f9226256998c698eea15c5d108f7986a138d
disilvap <alienware.matrix@gmail.com>
2020-04-14 07:29:23 -0500

    debug=false

--- coffee/settings.py
+++ coffee/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2q!9m8r-lb@un0e)%9x17)7z6nqsz1w2s#^lzq8btya!9=#l%^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
